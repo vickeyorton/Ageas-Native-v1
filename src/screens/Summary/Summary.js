fbc7d084988917449370d5ac26ff5aae9096caa7
Venkat1402 <venkateshwarreddy140299@gmail.com>
2021-08-11 10:54:05 +0530

    summary page changes updated

--- src/screens/Summary/Summary.js
+++ src/screens/Summary/Summary.js
@@ -1,25 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { Text, View, StyleSheet ,TouchableOpacity,ScrollView} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import {
   THANKYOU_PAGE,
   INVOLVED_PAGE,
   CONTACT_PAGE,
   INCIDENT_PAGE,
 } from '../../constants/routeNames';
-import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-// You can import from local files
 
-const Summary = ({navigation}) => {
+const Summary =({navigation}) => {
   const {setOptions, toggleDrawer} = useNavigation();
   useEffect(() => {
     setOptions({
@@ -45,30 +38,28 @@ const Summary = ({navigation}) => {
 
   return (
     <ScrollView style={styles.scrollview}>
-      <View style={styles.container}>
-        <View style={{padding: 10}}>
-          <Text style={{lineHeight: 22, fontSize: 16, marginBottom: 10}}>
+    <View style={styles.container}>
+      <Text style={styles.headingText}>
             You have now entered all of your claim details. Please review the
             details and once you are happy you can submit your claim.
-          </Text>
-        </View>
+      </Text>
         <View style={styles.summaryContent}>
-          <View style={styles.incidentSection}>
-            <Text style={{fontSize: 18}}>Incident details</Text>
+          <View style={styles.header}>
+           <Text style={{fontSize: 18 , color : "white"}}>Incident details</Text>
             <TouchableOpacity onPress={() => setShowIncident(!showIncident)}>
               {showIncident ? (
-                <AntDesign name="up" size={18} color="black" />
+                <AntDesign name="up" size={18} color="white" />
               ) : (
-                <AntDesign name="down" size={18} color="black" />
+                <AntDesign name="down" size={18} color="white" />
               )}
             </TouchableOpacity>
+        
           </View>
-          {showIncident ? (
-            <View style={{padding: 8}}>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Date and time</Text>
-                {/* <Text style={styles.value}>11/08/2021 and 11:20</Text> */}
-                <Text style={styles.value}>
+           {showIncident ? (
+            <View style={{ padding : 10}}>
+              <View style={{marginBottom: 7}}>
+                <Text style={{fontSize : 16}}>Date and time</Text>
+                <Text style={{fontSize : 16}}>
                   {incident ? incident.date : 'DD'}/
                   {incident ? incident.month : 'MM'}/
                   {incident ? incident.year : 'YYYY'}{' '}
@@ -76,137 +67,129 @@ const Summary = ({navigation}) => {
                   {incident ? incident.minute : 'M'}
                 </Text>
               </View>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Vehicle registration</Text>
-                <Text style={styles.value}>
-                  {' '}
-                  {incident ? incident.vehicleReg : '-'}
+              <View style={{marginBottom: 7}}>
+                <Text style={{fontSize : 16}}>Vehicle registration</Text>
+                <Text style={{fontSize : 16}}>
+                 {incident ? incident.vehicleReg : '-'} 
                 </Text>
               </View>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Circumstance</Text>
-                <Text style={styles.value}>
+              <View  style={styles.editContainer} >
+              <View>
+                <Text style={{fontSize : 16}}>Circumstance</Text>
+                <Text style={{fontSize : 16}}>
                   {incident ? incident.situation : '-'}
                 </Text>
               </View>
               <View style={styles.editSection}>
-                <TouchableOpacity>
-                  <Text
-                    style={styles.edit}
-                    onPress={() => navigation.navigate(INCIDENT_PAGE)}>
-                    Edit
-                  </Text>
+                <TouchableOpacity onPress={() => navigation.navigate(INCIDENT_PAGE)}>
+                  <Text style={styles.edit}>Edit</Text>
                 </TouchableOpacity>
+              </View>
               </View>
             </View>
           ) : null}
-          <View style={styles.line} />
-
-          <View style={styles.incidentSection}>
-            <Text style={{fontSize: 18}}>Contact details</Text>
-
+        </View>
+        <View style={styles.summaryContent}>
+          <View style={styles.header}>
+           <Text style={{fontSize: 18 , color : "white"}}>Contact details</Text>
             <TouchableOpacity onPress={() => setShowContact(!showContact)}>
               {showContact ? (
-                <AntDesign name="up" size={18} color="black" />
+                <AntDesign name="up" size={18} color="white" />
               ) : (
-                <AntDesign name="down" size={18} color="black" />
+                <AntDesign name="down" size={18} color="white" />
               )}
             </TouchableOpacity>
+        
           </View>
-
-          {showContact ? (
-            <View style={{padding: 8}}>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Home telephone number</Text>
-                <Text style={styles.value}>
+           {showContact ? (
+            <View style={{ padding : 10}}>
+              <View style={{marginBottom: 7}}>
+                <Text style={{fontSize : 16}}>Home telephone number</Text>
+                <Text style={{fontSize : 16}}>
                   {contactObj ? contactObj.HousePhone : '-'}
-                </Text>
+                 </Text>
               </View>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Mobile number</Text>
-                <Text style={styles.value}>
+              <View style={{marginBottom: 7}}>
+                <Text style={{fontSize : 16}}>Mobile number</Text>
+                <Text style={{fontSize : 16}}>
                   {contactObj ? contactObj.MobilePhone : '-'}
                 </Text>
               </View>
-              <View style={{marginBottom: 9}}>
-                <Text style={{fontSize : 17}}>Email address</Text>
-                <Text style={styles.value}>
-                  {contactObj ? contactObj.Email : '-'}
+              <View  style={styles.editContainer} >
+              <View>
+                <Text style={{fontSize : 16}}>Email address</Text>
+                <Text style={{fontSize : 16}}>
+                   {contactObj ? contactObj.Email : '-'}
                 </Text>
               </View>
               <View style={styles.editSection}>
-                <TouchableOpacity>
-                  <Text
-                    style={styles.edit}
-                    onPress={() => navigation.navigate(CONTACT_PAGE)}>
-                    Edit
-                  </Text>
+                <TouchableOpacity onPress={() => navigation.navigate(CONTACT_PAGE)}>
+                  <Text style={styles.edit}>Edit</Text>
                 </TouchableOpacity>
+              </View>
               </View>
             </View>
           ) : null}
-          <View style={styles.line} />
-          <View style={styles.incidentSection}>
-            <Text style={{fontSize: 18}}>Involved parties details</Text>
-
+        </View>
+        <View style={styles.summaryContent}>
+          <View style={styles.header}>
+           <Text style={{fontSize: 18 , color : "white"}}>Involved parties details</Text>
             <TouchableOpacity onPress={() => setShowInvolved(!showInvolved)}>
               {showInvolved ? (
-                <AntDesign name="up" size={18} color="black" />
+                <AntDesign name="up" size={18} color="white" />
               ) : (
-                <AntDesign name="down" size={18} color="black" />
+                <AntDesign name="down" size={18} color="white" />
               )}
             </TouchableOpacity>
+        
           </View>
-
-          {showInvolved ? (
-            <View style={{padding: 8}}>
-              <Text style={[{fontSize : 17},{marginBottom:5},{fontWeight:"bold"}]}>Your vehicle</Text>
-              <View style={styles.subSectionInvolved}>
-                <Text style={{fontSize : 16}}>No of passengers</Text>
-                {/* <Text style={styles.value}>{involved ? involved.noOfPassengers : "N/A"}</Text> */}
-                <Text style={styles.value}>
-                  {involvedObj ? involvedObj.noOfPassengers : '-'}
-                </Text>
+           {showInvolved ? (
+            <View style={{ padding : 10}}>
+              <View style={{marginBottom: 7}}>
+                <Text style={{fontSize : 16,color:"#8e419c"}}>Your vehicle</Text>
+                 <View style={styles.line1} />
+                <Text style={{fontSize : 16}}>No of passengers </Text>
+                <Text style={{fontSize : 16}}> 
+                   {involvedObj ? involvedObj.noOfPassengers : '-'}
+                 </Text>
               </View>
-              <Text style={[{fontSize : 17},{marginBottom:5},{fontWeight:"bold"}]}>Their vehicle</Text>
-              <View style={styles.subSectionInvolved}>
+              <View style={{marginBottom: 7}}>
+                 <Text style={{fontSize : 16,color:"#8e419c"}}>Their vehicle</Text>
+                 <View style={styles.line1} />
                 <Text style={{fontSize : 16}}>Registration</Text>
-                <Text style={styles.value}>NA</Text>
-              </View>
-              <View style={styles.subSectionInvolved}>
+                <Text style={{fontSize : 16,marginBottom: 9}}>NA</Text>
+              <View>
                 <Text style={{fontSize : 16}}>Driver name</Text>
-                <Text style={styles.value}>daniel</Text>
+                <Text style={{fontSize : 16}}>Daniel</Text>
               </View>
-              <View style={styles.subSectionInvolved}>
+              </View>
+              <View  style={styles.editContainer} >
+              <View>
                 <Text style={{fontSize : 16}}>No of passengers</Text>
-                {/* <Text style={styles.value}>{involved ? involved.noOfPassengers : "N/A"}</Text> */}
-                <Text style={styles.value}>
-                  {involvedObj ? involvedObj.noOfPassengers : '-'}
+                <Text style={{fontSize : 16}}> 
+                    {involvedObj ? involvedObj.noOfPassengers : '-'}
                 </Text>
               </View>
               <View style={styles.editSection}>
-                <TouchableOpacity>
-                  <Text
-                    style={styles.edit}
-                    onPress={() => navigation.navigate(INVOLVED_PAGE)}>
-                    Edit
-                  </Text>
+                <TouchableOpacity  onPress={() => navigation.navigate(INVOLVED_PAGE)}>
+                  <Text style={styles.edit}>Edit</Text>
                 </TouchableOpacity>
+              </View>
               </View>
             </View>
           ) : null}
         </View>
         <View style={styles.submitSection}>
           <TouchableOpacity
-            onPress={() => navigation.navigate(THANKYOU_PAGE)}
-            style={styles.submitButton}>
+            style={styles.submitButton}
+            onPress={() => navigation.navigate(THANKYOU_PAGE)}>
             <Text style={styles.submitButtonText}>Submit</Text>
           </TouchableOpacity>
         </View>
-      </View>
+    </View>
     </ScrollView>
   );
-};
+}
 
 export default Summary;
 
@@ -214,58 +197,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    padding: 8,
-  },
-  summaryContent: {
     padding: 15,
-    borderWidth: 1,
-    borderRadius: 2,
-    borderColor: '#ddd',
-    borderBottomWidth: 0,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 1,
   },
-  incidentSection: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
+  headingText: {
+    fontFamily: '',
+    fontSize: 18,
+    fontWeight: 'normal',
+    marginBottom: 15,
   },
-  value: {
-    fontSize: 15,
+  summaryContent : { 
+    borderWidth : 1,
+    borderColor : '#8e419c',
+    marginBottom : 8
+  },
+  header : {
+   justifyContent: 'space-between',
+   flexDirection: 'row', 
+   backgroundColor :  '#8e419c' ,
+   padding : 7
+  },
+  editContainer : {
+     justifyContent: 'space-between',
+     flexDirection: 'row', 
   },
   editSection: {
     borderWidth: 1,
     borderColor: '#8e419c',
-    marginLeft: 'auto',
-    // borderRadius : 15,
-    paddingTop: 5,
-    paddingBottom: 5,
-    width: 80,
-  },
+    width : 70,
+    height : 35,
+    paddingVertical : 5
+  }, 
   edit: {
     color: '#8e419c',
     textAlign: 'center',
+    fontSize : 16
   },
-  subSectionInvolved: {
-    paddingLeft: 10,
-    marginBottom: 9,
-  },
-  line: {
-    height: 1,
-    backgroundColor: 'rgb(192,192,192)',
-    alignSelf: 'stretch',
-    marginTop: 5,
-    marginBottom: 7,
-  },
-  scrollview: {
-    flexGrow: 1,
-  },
-  submitSection: {
-    marginTop: 20,
-  },
-  submitButton: {
+   submitButton: {
     borderWidth: 1,
     width: '100%',
     borderColor: '#fff',
@@ -281,4 +248,11 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '700',
   },
+  line1 : {
+    height: 1,
+    backgroundColor: "#8e419c",
+    alignSelf: 'stretch',
+    marginVertical : 5
+  },
+ 
 });
